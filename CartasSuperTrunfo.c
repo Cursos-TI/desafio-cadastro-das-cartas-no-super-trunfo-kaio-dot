ff13c364dc5851093ae4c70934849f81684c74c8
Kaio Lyan <85936598+kaio-dot@users.noreply.github.com>
2024-10-17 10:56:37 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+#include <locale.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    setlocale(LC_ALL, "");
+
+    char estado[100]; // Alterado para string
+    char codigoEstado[100];
+    char nomeCidade[100];
+    char escolha;
+    int populacao, pontoTurisitco;
+    float area, pib;
+
+    do {
+        printf("Nome do Estado: ");
+        scanf("%s", estado);
+        while (getchar() != '\n'); // Limpar o buffer corretamente
+        
+        printf("O código do estado: ");
+        scanf("%s", codigoEstado);
+        while (getchar() != '\n');
+        
+        printf("O nome da cidade: ");
+        scanf("%s", nomeCidade);
+        while (getchar() != '\n');
+        
+        printf("A população total: ");
+        scanf("%d", &populacao);
+        while (getchar() != '\n');
+        
+        printf("A quantidade de pontos turísticos: ");
+        scanf("%d", &pontoTurisitco);
+        while (getchar() != '\n');
+        
+        printf("A área da cidade: ");
+        scanf("%f", &area);
+        while (getchar() != '\n');
+        
+        printf("Por último, o PIB: ");
+        scanf("%f", &pib);
+        while (getchar() != '\n');
+
+        printf("\nTodas as informações:\n");
+        printf("O nome do estado: %s\n", estado);
+        printf("O código do estado: %s\n", codigoEstado);
+        printf("O nome da cidade: %s\n", nomeCidade);
+        printf("A quantidade da população: %d\n", populacao);
+        printf("A quantidade de pontos turísticos: %d\n", pontoTurisitco);
+        printf("A área total: %.1f\n", area);
+        printf("O PIB: %.1f\n", pib);
+
+        printf("\nInformações corretas? (Y/N): ");
+        scanf("%c", &escolha); // Alteração: adicionar o & na variável escolha
+        while (getchar() != '\n'); // Limpar o buffer após a leitura
+
+        if (escolha == 'Y' || escolha == 'y') {
+            return 0;
+        }
+    } while (1);
 
     return 0;
 }
